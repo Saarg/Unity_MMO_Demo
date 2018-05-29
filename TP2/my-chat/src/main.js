a46f2d73390d845f926e9f4e5d1ba601d9005dcb
Saarg <milsonneau.jean@free.fr>
2018-05-29 02:58:42 +0000

    added script for prod

--- TP2/my-chat/src/main.js
+++ TP2/my-chat/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueSocketio from 'vue-socket.io';
 
-Vue.use(VueSocketio, 'http://192.168.2.57:3000');
+Vue.use(VueSocketio, 'http://51.15.247.163:3000');
 
 Vue.config.productionTip = false
 
